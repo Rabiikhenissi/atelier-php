5f27fbcb23cb6b2fea6f3eb05216191e274b97b1
Rabii khenissi <rabiikhenissi124@gmail.com>
2023-09-28 23:32:55 +0100

    v2 ex4

--- ex3.php
+++ ex3.php
@@ -7,7 +7,7 @@ echo"<br>";
 echo $nom."<br>".$prenom;
 echo "<table border=1 ><tr><th>Nom</th><th>prenom</th></tr><tr><td>".$nom."</td><td>".$prenom."</td></tr>";
 $info=$nom." ".$prenom;
-echo "<script> alert (`${info}`);</script>";
+echo "<script> alert('${info}');</script>";
 ?>
 
 
